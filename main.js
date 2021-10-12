74aac516976555620e7eca6a4311a4fb68bc2304
SebaR <riolfosebastian@gmail.com>
2021-10-12 18:33:29 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,11 @@
+/* const app = document.getElementById('typewriter');
+
+const typewriter = new Typewritter(app, {
+    loop: true,
+    delay:75
+});
+
+typewriter
+    .typeString ('Sebastian Riolfo')
+    .pauseFor(200)
+    .start(); */
